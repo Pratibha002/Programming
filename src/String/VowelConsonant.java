7b32f0f1d40c4653b399eff13a1885d31dd80e94
Pratibha <pc634805@gmail.com>
2024-06-04 23:57:13 +0530

    Java Program to Count Vowels and Consonants in a String

--- src/String/VowelConsonant.java
+++ src/String/VowelConsonant.java
@@ -1,2 +1,25 @@
-package String;public class VowelConsonant {
+//Java Program to Count Vowels and Consonants in a String
+package String;
+import java.util.Scanner;
+public class VowelConsonant {
+    public static void main(String [] args){
+        //String s ="hello world";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String : " );
+        String s = sc.nextLine().toLowerCase();
+        int vowelCount=0;
+        int consonantCount=0;
+
+        for(int i=0; i<s.length(); i++){
+            char c=s.charAt(i);
+            if((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u' )){
+                vowelCount++;
+
+            }else {
+                consonantCount++;
+            }
+        }System.out.println("Vowels : " +vowelCount);
+        System.out.println("consonant : " +consonantCount);
+    }
 }
+
