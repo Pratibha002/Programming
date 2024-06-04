e5cdb0987d335cae0e84582193b4df9e4bc90260
Pratibha <pc634805@gmail.com>
2024-06-04 23:59:26 +0530

    Java Program to Count Number of Words in Given String

--- src/String/CountNumberOfWords.java
+++ src/String/CountNumberOfWords.java
@@ -1,2 +1,15 @@
-package String;public class CountNumberOfWords {
+//Java Program to Count Number of Words in Given String
+package String;
+import java.util.Scanner;
+public class CountNumberOfWords {
+
+    public static void main(String[] args){
+        // String s = "hello world hi java";
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String s =sc.nextLine();
+        String [] words = s.split(" ");
+        int count = words.length;
+        System.out.println("Number of words in string = " + count);
+    }
 }
