ccd0f7103b7664d9c93e8f1751d9411f8b962d40
Pratibha <pc634805@gmail.com>
2024-06-04 17:43:01 +0530

    first commit

--- src/String/HelloWorld.java
+++ src/String/HelloWorld.java
@@ -1,2 +1,7 @@
-package String;public class HelloWorld {
+package String;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Helloworld");
+    }
 }
