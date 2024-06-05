f27b4fc6cc5da7bda26da4e9ee764a26bb6c8870
Pratibha <pc634805@gmail.com>
2024-06-05 20:33:58 +0530

    Java Program to Count Occurrences of Words in a String

--- src/String/CountOccurrenceOfWord.java
+++ src/String/CountOccurrenceOfWord.java
@@ -1,2 +1,27 @@
-package String;public class CountOccurrenceOfWord {
+//Java Program to Count Occurrences of Words in a String
+package String;
+import java.util.Scanner;
+public class CountOccurrenceOfWord {
+    public static void main(String[] args) {
+        //String s = "hello java hello";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE STRING : ");
+        String s = sc.nextLine();
+        String[] words = s.split(" ");
+        //System.out.println("Duplicate words in the string:");
+        for (int i = 0; i < words.length; i++) {
+            int count = 0;
+            if (words[i].equals(" ")) {
+                continue;
+            }
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equalsIgnoreCase(words[j])) {
+                    count++;
+                    words[j] = " ";
+                }
+            }if (count > 0) {
+                System.out.println("Duplicate word :  " + words[i] + " -> " + (count + 1));
+            }
+        }sc.close();
+    }
 }
