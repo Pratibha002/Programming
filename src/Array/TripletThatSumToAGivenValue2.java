5b8168d823c9bfbfa0d42c2614e516df5ff965ff
Pratibha <pc634805@gmail.com>
2024-06-20 22:55:10 +0530

    Find the triplet that sum to a given value random approach

--- src/Array/TripletThatSumToAGivenValue2.java
+++ src/Array/TripletThatSumToAGivenValue2.java
@@ -1,2 +1,29 @@
-package Array;public class TripletThatSumToAGivenValue2 {
+package Array;
+import java.util.ArrayList;
+public class TripletThatSumToAGivenValue2 {
+    public static void main(String[] args){
+        ArrayList<Integer> arr = new ArrayList<Integer>();//it is a random approach
+        //{1, 4, 45, 6, 10, 8}
+        arr.add(1);
+        arr.add(4);
+        arr.add(45);
+        arr.add(6);
+        arr.add(10);
+        arr.add(8);
+        System.out.println("Given array :"+arr);
+        int X = 44;
+        boolean contains=false;
+        for(int i=0; i< arr.size(); i++){
+            for(int j=0; j< arr.size(); j++){
+                int required =X- (arr.get(i) + arr.get(j));
+                if(arr.contains(required)){
+                    contains=true;
+                }
+            }
+        }if(contains){
+            System.out.println("Triplet found !!");
+        }else {
+            System.out.println("Triplet not found !!");
+        }
+    }
 }
