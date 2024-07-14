0f4471ef0a3e1596a275c4fdede5d3c53ee7684b
Pratibha <pc634805@gmail.com>
2024-07-14 23:40:19 +0530

    Reverse a String

--- src/String/ReverseAString.java
+++ src/String/ReverseAString.java
@@ -1,2 +1,11 @@
-package String;public class ReverseAString {
+package String;
+
+public class ReverseAString {
+    public static void main(String[] args){
+        String s="HELLO WORLD";
+        for(int i =s.length()-1 ; i>=0 ; i--){
+            System.out.println(s.charAt(i));
+
+        }
+    }
 }
