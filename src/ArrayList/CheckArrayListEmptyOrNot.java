7f414b7c7b9f51c18f613eda9ddbeb1e31cf9b47
Pratibha <pc634805@gmail.com>
2024-07-20 23:12:01 +0530

    Arraylist practice

--- src/ArrayList/CheckArrayListEmptyOrNot.java
+++ src/ArrayList/CheckArrayListEmptyOrNot.java
@@ -1,2 +1,17 @@
-package ArrayList;public class CheckArrayListEmptyOrNot {
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class CheckArrayListEmptyOrNot {
+    public static void main(String[] args) {
+        ArrayList <Integer> al =new ArrayList<>();
+        ArrayList <Integer> al2 =new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        System.out.println("Check ArrayList 1 is empty : "+al.isEmpty());
+        System.out.println("Check ArrayList 2 is empty : "+al2.isEmpty());
+
+    }
 }
