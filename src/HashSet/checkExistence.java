5ad6cd52c8d27ff3d547557e2123fd76efbbb7a6
Pratibha <pc634805@gmail.com>
2024-07-22 23:25:23 +0530

    HashSetPractice

--- src/HashSet/checkExistence.java
+++ src/HashSet/checkExistence.java
@@ -1,2 +1,14 @@
-package HashSet;public class checkExistence {
+package HashSet;
+
+import java.util.HashSet;
+
+public class checkExistence {
+    public static void main(String[] args) {
+        HashSet<String> h=new HashSet<>();
+        h.add("apple");
+        h.add("banana");
+        h.add("orange");
+        System.out.println(h.contains("banana"));
+
+    }
 }
