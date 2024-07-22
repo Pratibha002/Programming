007bc071b1e15205c55804dd1690b1ebfbad65e4
Pratibha <pc634805@gmail.com>
2024-07-22 23:24:30 +0530

    HashSetPractice

--- src/HashSet/SizeAndClear.java
+++ src/HashSet/SizeAndClear.java
@@ -1,2 +1,17 @@
-package HashSet;public class SizeAndClear {
+package HashSet;
+
+import java.util.HashSet;
+
+public class SizeAndClear {
+    public static void main(String[] args) {
+        HashSet<Double> h=new HashSet<>();
+        h.add(1.1);
+        h.add(2.2);
+        h.add(3.3);
+        h.add(4.4);
+        h.add(5.5);
+        System.out.println("Size of the HashSet : "+h.size());
+        h.clear();
+        System.out.println(h);
+    }
 }
