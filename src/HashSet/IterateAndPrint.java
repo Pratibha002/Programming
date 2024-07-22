10cb226d076a9cb26332c3635a2395ac48fedebd
Pratibha <pc634805@gmail.com>
2024-07-22 23:23:53 +0530

    HashSetPractice

--- src/HashSet/IterateAndPrint.java
+++ src/HashSet/IterateAndPrint.java
@@ -1,2 +1,17 @@
-package HashSet;public class IterateAndPrint {
+package HashSet;
+
+import java.util.HashSet;
+
+public class IterateAndPrint {
+    public static void main(String[] args) {
+        HashSet<Character> h=new HashSet<>();
+        h.add('a');
+        h.add('b');
+        h.add('c');
+        h.add('d');
+        h.add('e');
+        for(Character c:h){
+            System.out.println(c);
+        }
+    }
 }
