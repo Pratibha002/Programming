0d0212a1fefdb2554d851f9672570e3cf62f4be6
Pratibha <pc634805@gmail.com>
2024-07-22 23:24:56 +0530

    HashSetPractice

--- src/HashSet/UnionOfSets.java
+++ src/HashSet/UnionOfSets.java
@@ -1,2 +1,22 @@
-package HashSet;public class UnionOfSets {
+package HashSet;
+
+import java.util.HashSet;
+
+public class UnionOfSets {
+    public static void main(String[] args) {
+        HashSet<Integer> h=new HashSet<>();
+        h.add(1);
+        h.add(2);
+        h.add(3);
+        System.out.println("Hashset"+h);
+        HashSet<Integer> h2=new HashSet<>(h);
+        h2.add(4);
+        h2.add(5);
+        h2.add(6);
+        System.out.println("Hashset2"+h2);
+
+
+
+
+    }
 }
