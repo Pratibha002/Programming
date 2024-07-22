be7a798456e4f74289a5ed030acc1a9767d002f6
Pratibha <pc634805@gmail.com>
2024-07-22 23:23:08 +0530

    HashSetPractice

--- src/HashSet/IntersectionOfSets.java
+++ src/HashSet/IntersectionOfSets.java
@@ -1,2 +1,17 @@
-package HashSet;public class IntersectionOfSets {
+package HashSet;
+
+import java.util.HashSet;
+
+public class IntersectionOfSets {
+    public static void main(String[] args) {
+        HashSet<Integer> h=new HashSet<>();
+        h.add(1);
+        h.add(2);
+        h.add(3);
+        System.out.println("Hashset"+h);
+        HashSet<Integer> h2=new HashSet<>();
+        h2.add(3);
+        h2.add(2);
+        h2.add(4);
+    }
 }
