edcc199729cdedc725c46b529e1edf1204fe7b5f
Pratibha <pc634805@gmail.com>
2024-07-23 21:47:45 +0530

    ArrayList Practice

--- src/ArrayList/ReplaceElement.java
+++ src/ArrayList/ReplaceElement.java
@@ -1,2 +1,18 @@
-package ArrayList;public class ReplaceElement {
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ReplaceElement {
+    public static void main(String[] args) {
+        ArrayList<Object> al = new ArrayList<>();
+        al.add(1);
+        al.add(1.1);
+        al.add("C");
+        al.add("hello");
+        al.set(0,"change");
+        System.out.println("Modified ArrayList " +al);
+        al.set(2,5);
+        System.out.println("Modified ArrayList " +al);
+
+    }
 }
