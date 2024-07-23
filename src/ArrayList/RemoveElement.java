e43c9ab55895dea4a550f58f432ce83d463b8980
Pratibha <pc634805@gmail.com>
2024-07-23 21:47:11 +0530

    ArrayList Practice

--- src/ArrayList/RemoveElement.java
+++ src/ArrayList/RemoveElement.java
@@ -1,2 +1,19 @@
-package ArrayList;public class RemoveElement {
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> al =new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        System.out.println("Before remove an Element : "+al);
+        al.remove(1);
+        System.out.println("After remove an Element : "+al);
+        int indexToRemove2 = 2;
+        al.remove(indexToRemove2);
+        System.out.println("After remove an Element at index " + indexToRemove2 + " : " + al);
+    }
 }
