bd82ae84d63a320e93534a3ae54b12e97448e55d
Pratibha <pc634805@gmail.com>
2024-07-25 20:14:13 +0530

    Hash Map Practice

--- src/HashMap/IterateTheHashMap.java
+++ src/HashMap/IterateTheHashMap.java
@@ -1,21 +1,25 @@
 package HashMap;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class FrquencyCounting {
+public class IterateTheHashMap {
     public static void main(String[] args) {
         HashMap<Integer, String> h =new HashMap<>();
         h.put(23, "abc");
         h.put(43, "dsf");
         h.put(34,"pqr");
         h.put(90,"xyz");
-        HashMap<Integer, Integer> frquency=new HashMap<>();
-        
-        for(int i : h.keySet()){
-            System.out.println(i);
-            frquency++;
 
+
+        for(Map.Entry<Integer,String> i : h.entrySet()){
+            System.out.println("Key : "+i.getKey()+" , Value : "+i.getValue());
         }
-        System.out.println( frquency);
+        //In Java, Map.Entry is an inner interface of the Map interface.
+        // It represents a key-value pair within a Map. Key and Value collectively callled as 'Entry'
+        // This interface provides methods to retrieve both the key and the value associated with that entry.
+
+        h.forEach((k,v) -> {System.out.println(k + "->" + v);});
+
     }
 }
