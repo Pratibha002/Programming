a8be9dca63a5fc15b5029d9c8b57864b133de9dd
Pratibha <pc634805@gmail.com>
2024-07-26 23:35:50 +0530

    Hash Map Practice

--- src/HashMap/RemoveEntrySet2.java
+++ src/HashMap/RemoveEntrySet2.java
@@ -1,2 +1,26 @@
-package HashMap;public class RemoveEntrySet2 {
+package HashMap;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RemoveEntrySet2 {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter the Key :" );
+        int key=s.nextInt();
+        System.out.println("Enter the value :" );
+        int value=s.nextInt();
+        HashMap<Integer,Integer> h = new HashMap<>();
+        h.put(1,11);
+        h.put(2,22);
+        h.put(3,22);
+        h.put(8,22);
+        h.put(4,44);
+        h.put(5,55);
+
+        if(h.containsValue(value)&& h.containsKey(key)){
+            h.remove(key,value);
+        }
+        System.out.println(h);
+
+    }
 }
