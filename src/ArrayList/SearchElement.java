1fffd3fd0eea03d1c2825e1c31dd33fdeebafd79
Pratibha <pc634805@gmail.com>
2024-07-27 23:21:17 +0530

    ArrayList practice

--- src/ArrayList/SearchElement.java
+++ src/ArrayList/SearchElement.java
@@ -1,2 +1,19 @@
-package ArrayList;public class SearchElement {
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class SearchElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> al =new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+      if(al.contains(2)){
+          System.out.println("Element found");
+      }else{
+          System.out.println("Element not found");
+      }
+
+    }
 }
