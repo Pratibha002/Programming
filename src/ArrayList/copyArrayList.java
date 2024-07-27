923239db00b7db16bbaa556c474d606a35b5da96
Pratibha <pc634805@gmail.com>
2024-07-27 23:22:17 +0530

    ArrayList practice

--- src/ArrayList/copyArrayList.java
+++ src/ArrayList/copyArrayList.java
@@ -1,2 +1,23 @@
-package ArrayList;public class copyArrayList {
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class copyArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Object> al = new ArrayList<>();
+        al.add(1);
+        al.add(1.1);
+        al.add('a');
+        al.add("hello");
+        System.out.println("Original ArrayList: " + al);
+        ArrayList<Object> copy = new ArrayList<>(al);
+
+        System.out.println("Copy of ArrayList: " + copy);
+    }
 }
+/*
+ ArrayList<Object> copy = new ArrayList<>(al);
+Shallow Copy: This approach creates a shallow copy of the ArrayList, meaning the elements themselves are not cloned.
+ Both al and copy will contain references to the same objects.
+ */
