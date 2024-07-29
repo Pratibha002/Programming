64e2f6cc80f933a68760e92437c4e348ec213a42
Pratibha <pc634805@gmail.com>
2024-07-29 21:12:29 +0530

    HashMap Practice

--- src/HashSet/IntersectionOfSets.java
+++ src/HashSet/IntersectionOfSets.java
@@ -4,14 +4,21 @@ import java.util.HashSet;
 
 public class IntersectionOfSets {
     public static void main(String[] args) {
+        int []a ={89, 24, 75, 11, 23};
+        int []b ={89, 2, 4};
         HashSet<Integer> h=new HashSet<>();
-        h.add(1);
-        h.add(2);
-        h.add(3);
-        System.out.println("Hashset"+h);
-        HashSet<Integer> h2=new HashSet<>();
-        h2.add(3);
-        h2.add(2);
-        h2.add(4);
+        HashSet<Integer> intersectionSet=new HashSet<>();
+
+        for(int i=0; i< a.length; i++){
+            h.add(a[i]);
+        }
+        for(int j=0; j<b.length; j++){
+            if(h.contains(b[j])){
+                intersectionSet.add(b[j]);
+            }
+        }
+        System.out.println("IntersectionSet  : "+intersectionSet);
+        System.out.println("Size of IntersectionSet : "+intersectionSet.size());
+
     }
 }
