b98a56add13b2ea79d82f428ca98d8adc56abdc8
Pratibha <pc634805@gmail.com>
2024-07-30 21:30:35 +0530

    Program to find all the pairs of key element

--- src/HashMap/PairWithSum.java
+++ src/HashMap/PairWithSum.java
@@ -1,2 +1,21 @@
-package HashMap;public class PairWithSum {
+package HashMap;
+
+public class PairWithSum {
+    public static void main(String[] args) {
+        int []arr ={1,2,3,4,5,6,7};
+        int key = 7;
+        for (int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i]+arr[j]==key){
+                    System.out.println(arr[i]+" "+arr[j]);
+
+                }
+
+            }
+
+        }
+
+    }
 }
+
+//9753776018
