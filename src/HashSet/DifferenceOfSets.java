d86c6a7952e3f4ed847c69801e3d92248c6ed989
Pratibha <pc634805@gmail.com>
2024-08-02 22:54:14 +0530

    HashSet Practice

--- src/HashSet/DifferenceOfSets.java
+++ src/HashSet/DifferenceOfSets.java
@@ -1,2 +1,24 @@
-package HashSet;public class DifferenceOfSets {
+package HashSet;
+
+import java.util.HashSet;
+
+public class DifferenceOfSets {
+    public static void main(String[] args) {
+        int[]a ={1,2,3,4,5,6};
+        int[]a1={3,4,5,6,7};
+
+        HashSet<Integer> h =new HashSet<>();
+        for(int i : a){
+            h.add(i);
+        }
+        System.out.println(h);
+        HashSet<Integer> h1 =new HashSet<>();
+        for(int i : a1){
+            h1.add(i);
+        }
+        System.out.println(h1);
+        HashSet<Integer> h2 =new HashSet<>(h);//difference
+        h2.removeAll(h1);
+        System.out.println("Difference of two sets : "+h2);
+    }
 }
