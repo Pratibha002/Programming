6c2056775b8fca0300b398638ea424f1f4732488
Pratibha <pc634805@gmail.com>
2024-08-02 22:57:11 +0530

    HashSet Practice

--- src/HashSet/checkIsSetEmpty.java
+++ src/HashSet/checkIsSetEmpty.java
@@ -1,2 +1,16 @@
-package HashSet;public class checkIsSetEmpty {
+package HashSet;
+
+import java.util.HashSet;
+
+public class checkIsSetEmpty {
+    public static void main(String[] args) {
+        HashSet<Integer> h=new HashSet<>();
+        HashSet<Integer> h2=new HashSet<>();
+        h.add(1);
+        h.add(2);
+        h.add(3);
+        System.out.println("Is hashset empty? "+h.isEmpty());
+        System.out.println("Is hashset empty? "+h2.isEmpty());
+
+    }
 }
