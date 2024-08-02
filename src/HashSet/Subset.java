1da014bb958b0da3e4a91bbe503ca8961c5b7cae
Pratibha <pc634805@gmail.com>
2024-08-02 22:55:48 +0530

    HashSet Practice

--- src/HashSet/Subset.java
+++ src/HashSet/Subset.java
@@ -1,2 +1,24 @@
-package HashSet;public class Subset {
+package HashSet;
+
+import java.util.HashSet;
+
+public class Subset {
+    public static void main(String[] args) {
+        int[]a ={1,2,3,4,5,6};
+        int[]a1={3,4,5,6,7};
+
+        HashSet<Integer> h =new HashSet<>();
+        for(int i : a){
+            h.add(i);
+        }
+        System.out.println(h);
+        HashSet<Integer> h1 =new HashSet<>();
+        for(int i : a1){
+            h1.add(i);
+        }
+        System.out.println(h1);
+
+        boolean subSet= h.containsAll(h1);
+        System.out.println("SubSet of another set : "+subSet);
+    }
 }
