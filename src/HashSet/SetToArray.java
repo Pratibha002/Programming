72ab08bbf258c8555720fe686c0fb1b27ca34f94
Pratibha <pc634805@gmail.com>
2024-08-02 22:53:41 +0530

    HashSet Practice

--- src/HashSet/SetToArray.java
+++ src/HashSet/SetToArray.java
@@ -1,2 +1,26 @@
-package HashSet;public class SetToArray {
+package HashSet;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class SetToArray {
+    public static void main(String[] args) {
+        HashSet<String> h =new HashSet<>();
+        h.add("apple");
+        h.add("mango");
+        h.add("kiwi");
+        h.add("banana");
+        h.add("berries");
+        System.out.println("Set : "+h);
+
+        System.out.println("Array Elements : ");
+        String [] s =h.toArray(new String[0]);
+        for(String i : s){
+            System.out.print(i+" ");
+        }
+        
+
+
+
+    }
 }
