75bf55ef95b2920ef9e6d7946b9d729337ba72f3
Pratibha <pc634805@gmail.com>
2024-08-03 23:26:38 +0530

    HashSet Practice

--- src/HashSet/UniqueElements.java
+++ src/HashSet/UniqueElements.java
@@ -1,2 +1,28 @@
-package HashSet;public class UniqueElements {
+package HashSet;
+
+import java.util.HashSet;
+
+public class UniqueElements {
+    public static void main(String[] args) {
+        HashSet<Integer> h=new HashSet<>();
+        HashSet<Integer> h1=new HashSet<>();
+        h.add(1);
+        h.add(2);
+        h.add(3);
+        System.out.println("FIRST SET : "+h);
+        h1.add(3);
+        h1.add(4);
+        h1.add(5);
+        System.out.println("SECOND SET : "+h1);
+        HashSet<Integer> uniqueSet=new HashSet<>(h);//a-b
+        HashSet<Integer> uniqueSet2=new HashSet<>(h1);//symmetric difference b-a
+        uniqueSet.removeAll(h1);
+        uniqueSet2.removeAll(h);
+
+        System.out.println("unique set : "+uniqueSet);
+        System.out.println("unique set : "+uniqueSet2);
+
+
+
+    }
 }
