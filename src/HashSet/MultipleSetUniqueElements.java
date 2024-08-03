ee8679897c1ed78f4e6b2bd9b1de4cc3b3af276e
Pratibha <pc634805@gmail.com>
2024-08-03 23:22:26 +0530

    HashSet Practice

--- src/HashSet/MultipleSetUniqueElements.java
+++ src/HashSet/MultipleSetUniqueElements.java
@@ -1,2 +1,32 @@
-package HashSet;public class MultipleSetUniqueElements {
+package HashSet;
+
+import java.util.HashSet;
+
+public class MultipleSetUniqueElements {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] a1 = {3, 4, 5, 6, 7};
+        int[] a2 = {5, 6, 7, 8, 9};
+
+        HashSet<Integer> h = new HashSet<>();
+        HashSet<Integer> h1 = new HashSet<>();
+        HashSet<Integer> h2 = new HashSet<>();
+        for(int i : a){
+            h.add(i);
+        }
+        System.out.println("SET 1 : " +h);
+        for(int i : a1){
+            h1.add(i);
+        }
+        System.out.println("SET 2 : " +h1);
+        for(int i : a2){
+            h2.add(i);
+        }
+        System.out.println("SET 3 : " +h2);
+        HashSet<Integer> unionSet = new HashSet<>(h);
+        unionSet.addAll(h1);
+        unionSet.addAll(h2);
+        System.out.println("UNION SET : "+unionSet);
+
+    }
 }
