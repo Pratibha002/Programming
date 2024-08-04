fb8ee0bc27e7478c80cfb3d6a561be5a17184c2d
Pratibha <pc634805@gmail.com>
2024-08-04 23:53:05 +0530

    Write a program to remove duplicates from an ArrayList using a HashSet.

--- src/ArrayListAndHashSet/RemoveDuplicates.java
+++ src/ArrayListAndHashSet/RemoveDuplicates.java
@@ -1,2 +1,23 @@
-package ArrayListAndHashSet;public class RemoveDuplicates {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> a =new ArrayList<>();
+        a.add(1);
+        a.add(1);
+        a.add(3);
+        a.add(4);
+        a.add(4);
+        a.add(5);
+        a.add(2);
+        System.out.println("ArrayList : "+a);
+        HashSet<Integer> h =new HashSet<>();
+        for(int i:a){
+            h.add(i);
+
+        }System.out.println("After removing the duplicates: "+h);
+    }
 }
