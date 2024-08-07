6d5f81f22e91e7eb41ea75908a372573f32a1cef
Pratibha <pc634805@gmail.com>
2024-08-07 23:44:29 +0530

    Write a program to merge two ArrayList objects into a single ArrayList without duplicates using a HashSet

--- src/ArrayListAndHashSet/MergeTwoArrayList.java
+++ src/ArrayListAndHashSet/MergeTwoArrayList.java
@@ -1,2 +1,34 @@
-package ArrayListAndHashSet;public class MergeTwoArrayList {
+//merge two arraylist without duplicates using hashset
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MergeTwoArrayList {
+    public static void main(String[] args) {
+        ArrayList<Character> a =new ArrayList<>();
+        ArrayList<Character> a1 =new ArrayList<>();
+
+        a.add('A');
+        a.add('B');
+        a.add('c');
+        a.add('d');
+        a.add('a');
+        a.add('B');
+        System.out.println("First Arraylist : " +a);
+        a1.add('a');
+        a1.add('b');
+        a1.add('C');
+        a1.add('d');
+        a1.add('C');
+        a1.add('b');
+        System.out.println("Second Arraylist : " +a1);
+
+        HashSet<Character> h= new HashSet<>(a);
+        h.addAll(a1);
+        System.out.println("Hashset : " +h);
+        ArrayList<Character>a3=new ArrayList<>(h);
+        System.out.println("Mergerd Arraylist without duplicates : "+a3);
+
+    }
 }
