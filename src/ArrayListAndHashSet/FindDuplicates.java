5cfe3a63c27934f34d5671a400de9cc06029eab3
Pratibha <pc634805@gmail.com>
2024-08-08 22:41:07 +0530

    Write a program to find duplicates in an ArrayList and list them using a HashSet.

--- src/ArrayListAndHashSet/FindDuplicates.java
+++ src/ArrayListAndHashSet/FindDuplicates.java
@@ -1,2 +1,29 @@
-package ArrayListAndHashSet;public class FindDuplicates {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class FindDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+
+        a.add(1); a.add(2); a.add(1); a.add(11); a.add(4);
+        a.add(3); a.add(11); a.add(2); a.add(5);
+
+        System.out.println("ArrayList : "+a);
+
+        HashSet<Integer> h =new HashSet<>();
+        HashSet<Integer>duplicateElements  =new HashSet<>(h);
+
+        for (Integer i:a){
+            if(!h.add(i)){
+                duplicateElements.add(i);
+            }
+        }
+        System.out.println(h);
+        System.out.println("Duplicate Elements : "+duplicateElements);
+
+
+    }
 }
+//07314998602
