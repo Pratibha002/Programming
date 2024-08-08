0386de6176ce893d24d3fc2b02f28c91e091fca2
Pratibha <pc634805@gmail.com>
2024-08-08 22:43:02 +0530

    Write a program to find the intersection of multiple ArrayList objects using HashSet.

--- src/ArrayListAndHashSet/Intersection2.java
+++ src/ArrayListAndHashSet/Intersection2.java
@@ -1,2 +1,33 @@
-package ArrayListAndHashSet;public class Intersection2 {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Intersection2 {
+    public static void main(String[] args) {
+        List<ArrayList<Character>>list  =new ArrayList<>();
+
+        ArrayList<Character> a=new ArrayList<>();
+        ArrayList<Character> a1=new ArrayList<>();
+        ArrayList<Character> a2=new ArrayList<>();
+
+        a.add('A'); a.add('B'); a.add('c'); a.add('d'); a.add('a'); a.add('B');
+        a1.add('a'); a1.add('b'); a1.add('C'); a1.add('d'); a1.add('C'); a1.add('b');
+        a2.add('a'); a2.add('B'); a2.add('C'); a2.add('d');
+        System.out.println("First ArrayList : "+a);
+        System.out.println("First ArrayList : "+a1);
+        System.out.println("First ArrayList : "+a2);
+
+        list.add(a);
+        list.add(a1);
+        list.add(a2);
+        System.out.println("list : "+list);
+
+        HashSet<Character> intersection = new HashSet<>(list.get(0));
+        for(ArrayList<Character> i : list){
+            intersection.retainAll(i);
+        }
+        System.out.println("Intersection of all lists: " + intersection);
+    }
 }
