1855382ab06993a1d119c5fc53921188376d025a
Pratibha <pc634805@gmail.com>
2024-08-08 22:42:29 +0530

    Write a program to find the intersection of multiple ArrayList objects using HashSet.

--- src/ArrayListAndHashSet/Intersection.java
+++ src/ArrayListAndHashSet/Intersection.java
@@ -1,2 +1,33 @@
-package ArrayListAndHashSet;public class Intersection {
+//merge two arraylist without duplicates using hashset
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Intersection {
+    public static void main(String[] args) {
+        ArrayList<Character> a =new ArrayList<>();
+        ArrayList<Character> a1 =new ArrayList<>();
+
+        a.add('A');
+        a.add('B');
+        a.add('c');
+        a.add('d');
+        a.add('a');
+        a.add('B');
+        System.out.println("First Arraylist : " +a);
+        a1.add('a');
+        a1.add('b');
+        a1.add('C');
+        a1.add('d');
+        a1.add('C');
+        a1.add('b');
+        System.out.println("Second Arraylist : " +a1);
+
+        HashSet<Character> h= new HashSet<>(a);
+        h.retainAll(a1);
+        System.out.println("Hashset : " +h);
+
+
+    }
 }
