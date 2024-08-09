62b2db4a9dea21a0ca514b889eed45a48d4ebdf7
Pratibha <pc634805@gmail.com>
2024-08-09 23:39:02 +0530

    Sort ArrayList and Remove Duplicates

--- src/ArrayListAndHashSet/RemoveOccurrenceOfElements.java
+++ src/ArrayListAndHashSet/RemoveOccurrenceOfElements.java
@@ -1,2 +1,15 @@
-package ArrayListAndHashSet;public class RemoveOccurrenceOfElements {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class RemoveOccurrenceOfElements {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);  a.add(2);  a.add(1);  a.add(3);  a.add(1); a.add(4);  a.add(1);
+        System.out.println("ArrayList: "+a);
+        HashSet<Integer>h= new HashSet<>(a);
+        System.out.println("Hashset: "+h);
+
+    }
 }
--- src/ArrayListAndHashSet/SortArraylistAndRemoveDuplicates.java
+++ src/ArrayListAndHashSet/SortArraylistAndRemoveDuplicates.java
@@ -1,2 +1,22 @@
-package ArrayListAndHashSet;public class SortArraylistAndRemoveDuplicates {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class SortArraylistAndRemoveDuplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> a= new ArrayList<>();
+        a.add(1); a.add(76); a.add(23); a.add(21); a.add(54); a.add(1); a.add(11);
+        a.add(2); a.add(2); a.add(22);
+        System.out.println("ArraList: "+a);
+
+        Collections.sort(a);
+        System.out.println("Sorted Arraylist: "+a);
+
+        HashSet<Integer> h= new HashSet<>(a);
+        System.out.println("Sorted and Unique HashSet: "+h);
+
+    }
 }
