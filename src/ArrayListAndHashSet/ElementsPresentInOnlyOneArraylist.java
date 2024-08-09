a82a422583b3f90f26df3f160fb582e7565447c8
Pratibha <pc634805@gmail.com>
2024-08-09 23:36:08 +0530

    Find the Difference Between Two ArrayLists

--- src/ArrayListAndHashSet/ElementsPresentInOnlyOneArraylist.java
+++ src/ArrayListAndHashSet/ElementsPresentInOnlyOneArraylist.java
@@ -1,2 +1,24 @@
-package ArrayListAndHashSet;public class ElementsPresentInOnlyOneArraylist {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class ElementsPresentInOnlyOneArraylist {
+    public static void main(String[] args) {
+        ArrayList<Character> a =new ArrayList<>();
+        ArrayList<Character> a1 =new ArrayList<>();
+
+        a.add('A'); a.add('B'); a.add('c'); a.add('d'); a.add('a'); a.add('B');  a.add('E');
+        System.out.println("First Arraylist : " +a);
+        a1.add('a'); a1.add('b'); a1.add('C'); a1.add('d'); a1.add('C'); a1.add('b');
+        System.out.println("Second Arraylist : " +a1);
+        HashSet<Character> h =new HashSet<>(a);
+        h.removeAll(a1);
+        System.out.println("Elements those are present only in first ArrayList : "+h);
+
+        HashSet<Character> h1 =new HashSet<>(a1);
+        h1.removeAll(a);
+        System.out.println("Elements those are present only in Second ArrayList : "+h1);
+
+    }
 }
