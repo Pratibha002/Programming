dd2c7c324ba6002377dd9b283dc812d65e0ab15b
Pratibha <pc634805@gmail.com>
2024-08-10 23:44:24 +0530

    find the first unique element in arraylist

--- src/ArrayListAndHashSet/FirstUniqueElement.java
+++ src/ArrayListAndHashSet/FirstUniqueElement.java
@@ -1,2 +1,33 @@
-package ArrayListAndHashSet;public class FirstUniqueElement {
+package ArrayListAndHashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class FirstUniqueElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1); a.add(76); a.add(23); a.add(21); a.add(54); a.add(1); a.add(11);
+        a.add(2); a.add(2); a.add(22);
+
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (Integer num : a) {
+            if (seen.contains(num)) {
+                duplicates.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        seen.removeAll(duplicates);
+
+        Integer firstUnique = null;
+        for (Integer num : a) {
+            if (seen.contains(num)) {
+                firstUnique = num;
+                break;
+            }
+        }
+        System.out.println("First unique element: " + firstUnique);
+    }
 }
