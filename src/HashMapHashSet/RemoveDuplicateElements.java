56fd9818826caf0c30185b3afa42cbfcae7a0f3a
Pratibha <pc634805@gmail.com>
2024-08-14 23:57:44 +0530

    Write a program to remove duplicate elements from multiple arrays using Set and a hashmap.

--- src/HashMapHashSet/RemoveDuplicateElements.java
+++ src/HashMapHashSet/RemoveDuplicateElements.java
@@ -9,31 +9,23 @@ public class RemoveDuplicateElements {
         int[] array3 = {7, 8, 9, 1};
 
         List<int[]> a = Arrays.asList(array1, array2, array3);
-
         HashSet<Integer> h = new HashSet<>();
-
-        // Using HashMap to store element counts
         HashMap<Integer, Integer> elementCountMap = new HashMap<>();
 
-        // Process each array
         for (int[] i : a) {
             for (int num : i) {
-                // Add to set (automatically handles duplicates)
                 h.add(num);
-
-                // Update element count in HashMap
                 elementCountMap.put(num, elementCountMap.getOrDefault(num, 0) + 1);
             }
         }
-
-        // Display unique elements
+        // HashSet
         System.out.println("Unique elements using Set:");
         for (Integer num : h) {
             System.out.print(num + " ");
         }
         System.out.println();
 
-        // Display elements with their counts using HashMap
+        //HashMap
         System.out.println("Element counts using HashMap:");
         for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
