ac58a972df8ec666e7ac3defb58f77cdabc0a8ac
Pratibha <pc634805@gmail.com>
2024-08-20 20:32:26 +0530

    Find the missing numbers in hash set

--- src/HashMapHashSet/FindMissingNumber.java
+++ src/HashMapHashSet/FindMissingNumber.java
@@ -1,2 +1,27 @@
-package HashMapHashSet;public class FindMissingNumber {
+package HashMapHashSet;
+import java.util.*;
+public class FindMissingNumber {
+    public static void main(String[] args) {
+
+        SortedSet<Integer>h= new TreeSet<>();
+        h.add(5);h.add(7);h.add(1);h.add(8);h.add(1);h.add(15);h.add(19);h.add(20);h.add(12);
+        System.out.println("Sorted Set: "+h);
+        System.out.println("lowest Element: "+h.first());
+        System.out.println("Highest Element: "+h.last());
+        System.out.println();
+        Integer lowest=h.first();
+        Integer highest=h.last();
+        HashSet<Integer>missingNum=new HashSet<>();
+        boolean isFound=false;
+        for(int i=lowest; i<=highest; i++){
+            if(!(h.contains(i))){
+               isFound=true;
+               missingNum.add(i);
+            }
+        }if(isFound){
+            System.out.println("Missing Numbers: "+missingNum);
+        }
+
+
+    }
 }
