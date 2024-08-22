c78da0b62384eb8508f7d13a06b6070d320a60d3
Pratibha <pc634805@gmail.com>
2024-08-22 23:53:35 +0530

    Write a program to remove all occurrences of a specific element from an ArrayList using a HashSet.

--- src/ArrayListAndHashSet/RemoveOccurrenceOfElements.java
+++ src/ArrayListAndHashSet/RemoveOccurrenceOfElements.java
@@ -2,14 +2,26 @@ package ArrayListAndHashSet;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class RemoveOccurrenceOfElements {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         ArrayList<Integer> a = new ArrayList<>();
         a.add(1);  a.add(2);  a.add(1);  a.add(3);  a.add(1); a.add(4);  a.add(1);
-        System.out.println("ArrayList: "+a);
-        HashSet<Integer>h= new HashSet<>(a);
-        System.out.println("Hashset: "+h);
+        System.out.println("Original ArrayList: "+a);
+        System.out.println("Enter the Element that we want to remove from ArrayList: ");
+        int Element=sc.nextInt();
+        HashSet<Integer>h= new HashSet<>();
+        h.add(Element);
+        ArrayList<Integer> finalList=new ArrayList<>();
+        for(int i:a){
+            if(!(h.contains(i))){
+                finalList.add(i);
+            }
+        }
+        System.out.println("ArrayList after removing the occurrence of the element: "+finalList);
+        sc.close();
 
     }
 }
