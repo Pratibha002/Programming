13fb90d332f7ddf13378fb2fb5ff42021509e47b
Pratibha <pc634805@gmail.com>
2024-08-25 23:58:37 +0530

    Compute Symmetric differecnce

---
+++ src/HashSet/SymmetricDifference.java
@@ -0,0 +1,22 @@
+package HashSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class SymmetricDifference {
+    public static void main(String[] args) {
+        HashSet<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        HashSet<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5, 6));
+        System.out.println("First HahSet: "+set1);
+        System.out.println("First HahSet: "+set2);
+
+        HashSet<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println("intersection"+intersection);
+
+        HashSet<Integer> symmetricDifference = new HashSet<>(set1);
+        symmetricDifference.removeAll(intersection);
+        System.out.println("symmetricDifference: "+symmetricDifference);
+
+    }
+}
