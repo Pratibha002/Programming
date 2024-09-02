04d7bff2812935828bc4e7623fb18674e1f012d2
Pratibha <pc634805@gmail.com>
2024-09-02 22:56:15 +0530

    program to find maximum and minimum element with the help of two pointers

--- src/Array/MaxAndMinWthPointer.java
+++ src/Array/MaxAndMinWthPointer.java
@@ -1,2 +1,35 @@
-package Array;public class MaxAndMinWthPointer {
+package Array;
+
+public class MaxAndMinWthPointer {
+    public static void main(String[] args) {
+        int [] a ={4,5,3,1,7,19,10,8,15};
+
+        int left=0;
+        int right=a.length-1;
+        int minElement=a[0];
+        int maxElement=a[0];
+
+        while (left<=right){
+            if(a[left]<minElement ){
+                minElement=a[left];
+
+            }
+            if (a[left]>maxElement){
+                maxElement=a[left];
+            }
+            if(a[right]<minElement ){
+                minElement=a[right];
+
+            }
+            if (a[right]>maxElement){
+                maxElement=a[right];
+            }
+            left++;
+            right--;
+
+
+        }
+        System.out.println("MINIMUM ELEMENT OF AN ARRAY: "+minElement);
+        System.out.println("MAXIMUM ELEMENT OF AN ARRAY: "+maxElement);
+    }
 }
