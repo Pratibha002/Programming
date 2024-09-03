d9ad2932b702c20811a6ca46b7b6142ae2a908c2
Pratibha <pc634805@gmail.com>
2024-09-03 22:40:25 +0530

    Move all negative numbers to beginning and positive to end with the help of pointers

--- src/Array/MoveNegativeNumbersWithPointers.java
+++ src/Array/MoveNegativeNumbersWithPointers.java
@@ -1,2 +1,33 @@
-package Array;public class MoveNegativeNumbersWithPointers {
+package Array;
+
+import java.util.Arrays;
+
+public class MoveNegativeNumbersWithPointers {
+    public static void main(String[] args) {
+        int [] a ={-12, 11, -13, -5, 6, -7, 5, -3, -6};
+
+        System.out.println("Original Array: "+Arrays.toString(a));
+
+        int left=0;
+        int right=a.length-1;
+
+        while (left<=right){
+            if (a[left]<0 && a[right]<0){
+                left++;
+            }
+            else if (a[left]>0 &&a[right]<0){
+            int temp=a[left];
+                a[left]=a[right];
+                a[right]=temp;
+                left++;
+                right--;
+            }else if (a[left]>0&&a[right]>0){
+                right--;
+            }else {
+                left++;
+                right--;
+            }
+        }
+        System.out.println("Sorted Array"+Arrays.toString(a));
+    }
 }
