4556a1307fd7a9459b932cee7a3fee0cd7f2784e
Pratibha <pc634805@gmail.com>
2025-01-02 23:00:43 +0530

    move negative numbers

--- src/TwoPointers/MoveNegativeNo.java
+++ src/TwoPointers/MoveNegativeNo.java
@@ -1,2 +1,31 @@
-package TwoPointers;public class MoveNegativeNo {
+package TwoPointers;
+//move all the negative elements to one side of the array
+public class MoveNegativeNo {
+    public static void main(String[] args) {
+        int []a = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+        int left=0;
+        int right=a.length-1;
+        int temp=0;
+
+        while (left<=right){
+            if (a[left]>0 && a[right]<0){
+                temp=a[left];
+                a[left]=a[right];
+                a[right]=temp;
+                left++;
+                right--;
+
+            }
+            if (a[left]<0){
+                left++;
+
+            }
+            if (a[right]>0){
+                right--;
+            }
+        }
+        for (int i=0; i<a.length; i++){
+            System.out.print(a[i]+ " ");
+        }
+    }
 }
