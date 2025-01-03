815b75c181df7bad5bb553daedc56ab976bdd852
Pratibha <pc634805@gmail.com>
2025-01-03 23:19:32 +0530

    LinearSearchOnStringArray

--- src/LinearSearch/LinearSearchOnStringArray.java
+++ src/LinearSearch/LinearSearchOnStringArray.java
@@ -2,25 +2,26 @@ package LinearSearch;
 
 import java.util.Scanner;
 
-public class LinearSearchOnArray {
+public class LinearSearchOnStringArray {
     public static  void  main(String[] args){
         int i;
         boolean found=false;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter how many elements do you want to enter: ");
         int n =sc.nextInt();
-        int [] a = new int[n];
+        sc.nextLine();// consume the leftover newline
+        String [] a = new String[n];
 
         System.out.println("Enter the elements: ");
 
         for ( i=0; i<n; i++){
-            a[i]=sc.nextInt();
+            a[i]=sc.nextLine();
         }
         System.out.println("Enter the item to find: " );
-        int item= sc.nextInt();
+        String item= sc.nextLine();
 
         for ( i=0; i<a.length; i++){
-            if (a[i]==item){
+            if (a[i].equals(item)){
                 found =true;
                 break;
             }
