b59f9054c7ed5c4a7fcafe10de29fa54badef8d0
Pratibha <pc634805@gmail.com>
2025-01-03 23:18:53 +0530

    LinearSearchOnLinkedList

--- src/LinearSearch/LinearSearchOnLinkedList.java
+++ src/LinearSearch/LinearSearchOnLinkedList.java
@@ -1,2 +1,35 @@
-package LinearSearch;public class LinearSearchOnLinkedList {
+package LinearSearch;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinearSearchOnLinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<String> LL= new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements you want to add on the linked list: ");
+        int n =sc.nextInt();
+
+        System.out.println("Enter the elements: ");
+
+        for (int i=0; i<n; i++){
+            String element = sc.nextLine();
+            LL.add(element);
+        }
+        System.out.println("Enter the element to find: ");
+        String item =sc.nextLine();
+
+//        int index =LL.linearSearch(item);
+//
+//        if (index!=-1){
+//            System.out.println("Element found at position: "+index);
+//        }else {
+//            System.out.println("Element not found!!");
+//        }
+
+//        public int linearSearch(String target){
+//
+//        }
+    }
 }
