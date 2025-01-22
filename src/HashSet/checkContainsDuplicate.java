cfef1d81bc304892070c35edfa2c482681f3732c
Pratibha <pc634805@gmail.com>
2025-01-22 08:30:18 +0530

    program to check contains duplicate or not

--- src/HashSet/checkContainsDuplicate.java
+++ src/HashSet/checkContainsDuplicate.java
@@ -1,2 +1,25 @@
-package HashSet;public class checkContainsDuplicate {
+package HashSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class checkContainsDuplicate {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>(Arrays.asList("apple", "banana", "cherry", "apple"));
+        System.out.println("Set: "+set);
+
+        HashSet<String> containsDuplicate = new HashSet<>();
+        boolean hasDuplicate=false;
+        for (String i: set){
+           if(!containsDuplicate.add(i)){
+               hasDuplicate=true;
+               break;
+           }
+        }
+        if (hasDuplicate){
+            System.out.println("Duplicate found");
+        }else{
+            System.out.println("Duplicate not found");
+        }
+    }
 }
