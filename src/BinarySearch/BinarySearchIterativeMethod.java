3a3897356074ebe1cd096bc2c4e1a6e663b6f431
Pratibha <pc634805@gmail.com>
2025-01-22 08:24:44 +0530

    binary search iterative method

--- src/BinarySearch/BinarySearchIterativeMethod.java
+++ src/BinarySearch/BinarySearchIterativeMethod.java
@@ -1,8 +1,44 @@
 package BinarySearch;
 
-public class BinarySearchRecursiveMethod {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchIterativeMethod {
     public static void main(String[] args) {
-        int [] a ={1,2,3,4,5,6,7,8,9,10};
-        
+        //for binary search array must be sorted.
+       // int [] a ={1,2,3,4,5,6,7,8,9,10};
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number of elements to be insert: ");
+        int n =sc.nextInt();
+        int [] a =new int[n];
+        System.out.println("Enter the elements: ");
+
+        for (int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.println("Sorted Array : "+Arrays.toString(a));
+        System.out.println("Enter the item to find: ");
+        int item = sc.nextInt();
+
+        int low=0;
+        int high= a.length-1;
+        int mid = (low+high)/2;
+
+        while (low<=high){
+            if (a[mid]==item){
+                System.out.println("Element found at position: "+mid);
+                break;
+            } else if (a[mid]<item) {
+                low=mid+1;
+            }else {
+                high=mid-1;
+            }
+            mid=(low+high)/2;
+        }
+        if (low>high){
+            System.out.println("Element not found!!");
+        }
+
     }
 }
