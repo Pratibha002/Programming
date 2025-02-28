461e7066de01106c632b54733010535d29659f60
Pratibha <pc634805@gmail.com>
2025-02-28 21:20:45 +0530

    transpose of matrix

--- src/Array2D/MatrixTranspose.java
+++ src/Array2D/MatrixTranspose.java
@@ -1,2 +1,28 @@
-package Array2D;public class MatrixTranspose {
+package Array2D;
+
+import java.util.Scanner;
+
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("ENTER THE NO. OF ROW AND COLUMN: ");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int [][] a = new int[rows][cols];
+        System.out.println("ENTER THE ARRAY ELEMENTS: ");
+        for (int i=0; i<rows; i++){
+            for (int j=0; j<cols; j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("TRANSPOSE OF MATRIX: ");
+        for (int j=0; j<cols; j++){
+            for (int i=0; i<rows; i++){
+                System.out.print(a[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
