16abf52cc470e0c8135d4372494ff259ade0386d
Pratibha <pc634805@gmail.com>
2025-06-23 23:43:32 +0530

    src/Recursion/NaturalNumberSum.java

--- src/Recursion/NaturalNumberSum.java
+++ src/Recursion/NaturalNumberSum.java
@@ -1,2 +1,24 @@
-package Recursion;public class NaturalNumberSum {
+package Recursion;
+
+import java.util.Scanner;
+
+//print the sum of first n natural number
+public class NaturalNumberSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER: ");
+        int n = sc.nextInt();
+        int sum =0;
+        int i = 1;
+        numSum(i,n,sum);
+    }
+    public static void numSum(int i, int n , int sum){
+        if (i==n){
+            sum+= i;
+            System.out.println("Sum of the natural numbers: "+sum);
+            return;
+        }
+        sum+=i;
+        numSum(i+1, n, sum);
+    }
 }
